d221b1ca770124aa16e507fbeeb2830178d30fcd
Md. Shehab Chowdhury <shehab15-11396@diu.edu.bd>
2022-10-12 13:08:02 +0600

    router complete

--- src/App.js
+++ src/App.js
@@ -1,11 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Graph from "./components/Graph/Graph";
+import Home from "./components/Home/Home";
+import MAin from "./layout/MAin";
+import Blog from "./components/Blog/Blog";
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <MAin></MAin>,
+      children: [
+        {
+          path: "/home",
+          element: <Home></Home>,
+        },
+
+        {
+          path: "/stat",
+          element: <Graph></Graph>,
+        },
+        {
+          path: "/blog",
+          element: <Blog></Blog>,
+        },
+        // {
+        //   path: "/fetch",
+        //   loader: async () => {
+        //     return fetch("");
+        //   },
+        //   element: ,
+        // },
+      ],
+    },
+    {
+      path: "*",
+      element: <div>404</div>,
+    },
+    // {
+    //   path: "/friend/:id",
+    //   loader: async ({ params }) => {
+    //     //console.log(params.id);
+    //     return fetch(`https://jsonplaceholder.typicode.com/users/${params.id}`);
+    //   },
+    //   element: ,
+    // },
+  ]);
   return (
-    <div className="App">
-      
-    </div>
+    <>
+      <div className="App"></div>
+      <RouterProvider router={router} />
+    </>
   );
 }
 
---
+++ src/components/Home/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Card from "../Card/Card";
+
+const Home = () => {
+  return (
+    <div className="grid  grid-cols-4 gap-4 my-10 mx-10">
+      <Card></Card>
+    </div>
+  );
+};
+
+export default Home;
